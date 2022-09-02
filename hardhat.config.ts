74b30af6217157ae63596b668879903c027a715f
livingrockrises <chirag@biconomy.io>
2022-09-02 14:55:36 +0400

    hardhat config updated with more networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
-import '@nomiclabs/hardhat-truffle5';
+import "@nomiclabs/hardhat-truffle5";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
@@ -177,6 +177,36 @@ const config: HardhatUserConfig = {
           ? [process.env.PRIVATE_KEY]
           : walletUtils.makeKeyList(),
       chainId: 42,
+    },
+    optimismGoerli: {
+      url: `https://goerli.optimism.io`,
+      accounts: walletUtils.makeKeyList(),
+      chainId: 420,
+      gasPrice: 6400000,
+    },
+    optimismMainnet: {
+      url: `https://mainnet.optimism.io`,
+      accounts: walletUtils.makeKeyList(),
+      chainId: 10,
+      // gasPrice: 6400000
+    },
+    celoTestnet: {
+      url: `https://alfajores-forno.celo-testnet.org`,
+      accounts: walletUtils.makeKeyList(),
+      chainId: 44787,
+      // gasPrice: 6400000
+    },
+    celoMainnet: {
+      url: `https://forno.celo.org`,
+      accounts: walletUtils.makeKeyList(),
+      chainId: 42220,
+      // gasPrice: 6400000
+    },
+    neonDevnet: {
+      url: `https://proxy.devnet.neonlabs.org/solana`,
+      accounts: walletUtils.makeKeyList(),
+      chainId: 245022926,
+      // gasPrice: 6400000
     },
   },
   gasReporter: {
