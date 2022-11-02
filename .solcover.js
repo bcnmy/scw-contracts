373a4d166ff308c8dcd985c81b9d04ce8782a02d
livingrockrises <chirag@biconomy.io>
2022-11-02 21:29:32 +0400

    init

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,9 @@
 module.exports = {
-    skipFiles: ['references/aa-4337/EntryPoint.sol']
-  };
+  configureYulOptimizer: true,
+  skipFiles: [
+  'references', 
+  'smart-contract-wallet/test',
+  'smart-contract-wallet/SmartWalletNoAuth.sol',
+  'smart-contract-wallet/utils'
+],
+};
