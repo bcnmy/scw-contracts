f44e1592bbaa90569fd10f4418a4534fa7ff2acd
livingrockrises <chirag@biconomy.io>
2022-11-02 15:09:07 +0400

    remediation HAL-09 and delete ECDSA from libs

--- contracts/smart-contract-wallet/paymasters/BasePaymaster.sol
+++ contracts/smart-contract-wallet/paymasters/BasePaymaster.sol
@@ -140,6 +140,6 @@ abstract contract BasePaymaster is IPaymaster {
 
     /// validate the call is made from a valid entrypoint
     function _requireFromEntryPoint() internal virtual {
-        require(msg.sender == address(entryPoint));
+        require(_msgSender() == address(entryPoint));
     }
 }
