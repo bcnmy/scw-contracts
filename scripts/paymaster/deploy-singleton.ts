1b4c416d4d1b75651ef32d76e63832a14b8714cf
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2022-11-14 23:46:08 +0400

    updated entry point address in paymaster script

--- scripts/paymaster/deploy-singleton.ts
+++ scripts/paymaster/deploy-singleton.ts
@@ -2,7 +2,7 @@ import hre, { ethers } from "hardhat";
 async function main() {
   const owner = "0x7306aC7A32eb690232De81a9FFB44Bb346026faB";
   const verifyingSigner = "0x416B03E2E5476B6a2d1dfa627B404Da1781e210d";
-  const entryPoint = "0xa948dc08f217C96c687dC3bF2E6051FB906b872A";
+  const entryPoint = "0x119df1582e0dd7334595b8280180f336c959f3bb";
 
   const VerifyingSingletonPaymaster = await ethers.getContractFactory(
     "VerifyingSingletonPaymaster"
