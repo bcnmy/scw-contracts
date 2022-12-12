f0a1563e26b364e17f330ef30a7aabe331120bbb
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2022-12-13 00:19:18 +0530

    updates for userOp utils and verification constructor params for verifying singleton paymaster

--- arguments.js
+++ arguments.js
@@ -1,9 +1,9 @@
 // for entrypoint verification
-module.exports = ["1000000000000000000", 86400];
+// module.exports = ["1000000000000000000", 86400];
 
 // Singleton paymaster
-/* module.exports = [
-  "0xa948dc08f217C96c687dC3bF2E6051FB906b872A",
-  "0x7306aC7A32eb690232De81a9FFB44Bb346026faB",
+module.exports = [
   "0x119df1582e0dd7334595b8280180f336c959f3bb",
-]; */
+  "0x7306aC7A32eb690232De81a9FFB44Bb346026faB",
+  "0x416B03E2E5476B6a2d1dfa627B404Da1781e210d",
+];
