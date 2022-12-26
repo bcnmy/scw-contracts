1a567ce10f6376c8d954dfd26cd2788743f5368b
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2022-12-26 13:40:03 +0400

    include coverage for entry point and paymaster

---
+++ contracts/smart-contract-wallet/aa-4337/test/TestExpiryAccount.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "../samples/SimpleAccount.sol";
+
+/**
+ * A test account, for testing expiry.
+ * add "temporary" owners, each with a deadline time for each.
+ * NOTE: this is not a full "session key" implementation: a real session key should probably limit
+ * other things, like target contracts and methods to be called.
+ */
+contract TestExpiryAccount is SimpleAccount {
+    using ECDSA for bytes32;
+
+    mapping(address => uint256) public ownerDeadlines;
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor(IEntryPoint anEntryPoint) SimpleAccount(anEntryPoint) {}
+
+    function initialize(address anOwner) public virtual override initializer {
+        super._initialize(anOwner);
+        addTemporaryOwner(anOwner, type(uint256).max);
+    }
+
+    function addTemporaryOwner(address owner, uint256 deadline) public onlyOwner {
+        ownerDeadlines[owner] = deadline;
+    }
+
+    /// implement template method of BaseAccount
+    function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash, address)
+    internal override view returns (uint256 deadline) {
+        bytes32 hash = userOpHash.toEthSignedMessageHash();
+        address signer = hash.recover(userOp.signature);
+        deadline = ownerDeadlines[signer];
+        require(deadline != 0, "account: wrong signature");
+        //not testing deadline (since we can't). just return it.
+    }
+}
---
+++ contracts/smart-contract-wallet/aa-4337/test/TestPaymasterAcceptAll.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "../core/BasePaymaster.sol";
+
+/**
+ * test paymaster, that pays for everything, without any check.
+ */
+contract TestPaymasterAcceptAll is BasePaymaster {
+
+    constructor(IEntryPoint _entryPoint) BasePaymaster(_entryPoint) {
+        // to support "deterministic address" factory
+        // solhint-disable avoid-tx-origin
+        if (tx.origin != msg.sender) {
+            _transferOwnership(tx.origin);
+        }
+    }
+
+    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint maxCost) external virtual override view
+    returns (bytes memory context, uint256 deadline) {
+        (userOp, userOpHash, maxCost);
+        return ("", 0);
+    }
+}
