c8290be4d88dc998fe7685d8837549ace162b154
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-01-10 18:55:22 +0400

    chore/optimize 0 address check on fallback handler

--- contracts/smart-contract-wallet/SmartAccount.sol
+++ contracts/smart-contract-wallet/SmartAccount.sol
@@ -168,10 +168,10 @@ contract SmartAccount is
         require(address(_entryPoint) == address(0), "Already initialized");
         require(_owner != address(0),"Invalid owner");
         require(_entryPointAddress != address(0), "Invalid Entrypoint");
-        require(_handler != address(0), "Invalid Entrypoint");
+        require(_handler != address(0), "Invalid Fallback Handler");
         owner = _owner;
         _entryPoint =  IEntryPoint(payable(_entryPointAddress));
-        if (_handler != address(0)) internalSetFallbackHandler(_handler);
+        internalSetFallbackHandler(_handler);
         setupModules(address(0), bytes(""));
     }
 
