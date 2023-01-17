222067f98c5489a102448721598e23527e110750
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-01-18 02:48:11 +0400

    updated deployment script

--- scripts/paymaster/deploy-singleton.ts
+++ scripts/paymaster/deploy-singleton.ts
@@ -1,8 +1,9 @@
 import hre, { ethers } from "hardhat";
 async function main() {
+  let tx, receipt;
   const owner = "0x7306aC7A32eb690232De81a9FFB44Bb346026faB";
   const verifyingSigner = "0x416B03E2E5476B6a2d1dfa627B404Da1781e210d";
-  const entryPoint = "0x20cfdbcd64cd85fd6af506dda4c12b25379481cf";
+  const entryPoint = "0xFF95ad8beD219969f608FfF36db647318F4bb4C0";
 
   const VerifyingSingletonPaymaster = await ethers.getContractFactory(
     "VerifyingSingletonPaymaster"
@@ -13,6 +14,12 @@ async function main() {
   );
   await singletonPaymster.deployed();
   console.log("singletonPaymster deployed at: ", singletonPaymster.address);
+
+  tx = await singletonPaymster.transferOwnership(owner);
+  receipt = await tx.wait(1);
+  console.log(
+    `Singleton verifying paymaster ownership transferred to ${owner}`
+  );
 }
 
 main().catch((error) => {
