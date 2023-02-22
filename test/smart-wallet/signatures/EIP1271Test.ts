ffa4b62b85a1a695f219393d8a0f2fa1f57c5c51
Filipp Makarov <filmakarov@Filipps-MacBook-Pro-2.local>
2023-02-22 11:38:41 +0300

    final tests + 0x vulner tests

--- test/smart-wallet/signatures/EIP1271Test.ts
+++ test/smart-wallet/signatures/EIP1271Test.ts
@@ -26,7 +26,7 @@ import {
   executeContractCallWithSigners,
   EOA_CONTROLLED_FLOW,
 } from "../../../src/utils/execution";
-import { buildMultiSendSafeTx } from "../../../src/utils/multisend";
+import { deployContract} from "../../utils/setupHelper";
 
 describe("EIP-1271 Signatures Tests", function () {
   let baseImpl: SmartWallet;
@@ -69,19 +69,19 @@ describe("EIP-1271 Signatures Tests", function () {
     const EntryPoint = await ethers.getContractFactory("EntryPoint");
     entryPoint = await EntryPoint.deploy();
     await entryPoint.deployed();
-    console.log("Entry point deployed at: ", entryPoint.address);
+    //console.log("Entry point deployed at: ", entryPoint.address);
 
     const DefaultHandler = await ethers.getContractFactory(
       "DefaultCallbackHandler"
     );
     handler = await DefaultHandler.deploy();
     await handler.deployed();
-    console.log("Default callback handler deployed at: ", handler.address);
+    // console.log("Default callback handler deployed at: ", handler.address);
 
     const BaseImplementation = await ethers.getContractFactory("SmartAccount");
     baseImpl = await BaseImplementation.deploy(entryPoint.address);
     await baseImpl.deployed();
-    console.log("base wallet impl deployed at: ", baseImpl.address);
+    //console.log("base wallet impl deployed at: ", baseImpl.address);
 
     const WalletFactory = await ethers.getContractFactory(
       "SmartAccountFactory"
@@ -91,30 +91,30 @@ describe("EIP-1271 Signatures Tests", function () {
       handler.address
     );
     await walletFactory.deployed();
-    console.log("wallet factory deployed at: ", walletFactory.address);
+    //console.log("wallet factory deployed at: ", walletFactory.address);
 
     const MockToken = await ethers.getContractFactory("MockToken");
     token = await MockToken.deploy();
     await token.deployed();
-    console.log("Test token deployed at: ", token.address);
+    //console.log("Test token deployed at: ", token.address);
 
     const Storage = await ethers.getContractFactory("StorageSetter");
     storage = await Storage.deploy();
-    console.log("storage setter contract deployed at: ", storage.address);
+    //console.log("storage setter contract deployed at: ", storage.address);
 
     const MultiSend = await ethers.getContractFactory("MultiSend");
     multiSend = await MultiSend.deploy();
-    console.log("Multisend helper contract deployed at: ", multiSend.address);
+    //console.log("Multisend helper contract deployed at: ", multiSend.address);
 
     const SignMessageLib = await ethers.getContractFactory("SignMessageLib");
     signMessageLib = await SignMessageLib.deploy();
 
-    console.log("mint tokens to owner address..");
+    //console.log("mint tokens to owner address..");
     await token.mint(owner, ethers.utils.parseEther("1000000"));
 
     let deployWalletIndex = 0;
 
-    console.log("Owner of Signer Smart Account is ", owner);
+    //console.log("Owner of Signer Smart Account is ", owner);
 
     // Deploy Signer Smart Account owned by Owner
     let signerSmartAccountAddress = await walletFactory.getAddressForCounterfactualWallet(owner, deployWalletIndex);
@@ -129,8 +129,6 @@ describe("EIP-1271 Signatures Tests", function () {
       signerSmartAccountAddress
     );
 
-    console.log("Signer smart account address %s %s", signerSmartAccountAddress, signerSmartAccount.address);
-
     // Deploy Main Smart Account owned by SignerSmartAccount
     let mainSmartAccountAddress = await walletFactory.getAddressForCounterfactualWallet(signerSmartAccountAddress, deployWalletIndex);
     
@@ -143,8 +141,6 @@ describe("EIP-1271 Signatures Tests", function () {
       "contracts/smart-contract-wallet/SmartAccount.sol:SmartAccount",
       mainSmartAccountAddress
     );
-
-    console.log("Main smart account address %s %s", mainSmartAccountAddress, mainSmartAccount.address);
 
     smartAccountInitialNativeTokenBalance = ethers.utils.parseEther("5");
 
@@ -217,7 +213,6 @@ describe("EIP-1271 Signatures Tests", function () {
         signature
       )
     ).to.emit(mainSmartAccount, "ExecutionSuccess");
-    
 
     expect(await token.balanceOf(charlie)).to.equal(
       tokensToBeTransferred
@@ -334,10 +329,261 @@ describe("EIP-1271 Signatures Tests", function () {
     expect(value).to.be.equal(eip1271MagicValue);
 
   });
-  
-  // Test that demonstrates cross SignerContracts replay attack, 
-  // when hacker manipulates the appended SignerContract address is not possible
-  // or if it is possible, it has almost no harm
+
+  it("Wont let the transaction to go through with manipulated signer contract address", async function () { 
+    
+    let deployWalletIndex = 1;
+
+    // Deploy Signer Smart Account 2 owned by Owner
+    let signerSmartAccount2Address = await walletFactory.getAddressForCounterfactualWallet(owner, deployWalletIndex);
+    
+    await walletFactory.deployCounterFactualWallet(
+        owner,
+        deployWalletIndex
+    );
+
+    let signerSmartAccount2 = await ethers.getContractAt(
+      "contracts/smart-contract-wallet/SmartAccount.sol:SmartAccount",
+      signerSmartAccount2Address
+    );
+
+    // Deploy Main Smart Account 2 owned by SignerSmartAccount 2
+    let mainSmartAccount2Address = await walletFactory.getAddressForCounterfactualWallet(signerSmartAccount2Address, deployWalletIndex);
+    
+    await walletFactory.deployCounterFactualWallet(
+      signerSmartAccount2Address,
+      deployWalletIndex
+    );
+
+    let mainSmartAccount2 = await ethers.getContractAt(
+      "contracts/smart-contract-wallet/SmartAccount.sol:SmartAccount",
+      mainSmartAccount2Address
+    );
+    
+    await token
+      .connect(accounts[0])
+      .transfer(mainSmartAccount.address, ethers.utils.parseEther("100"));
+    
+    await token
+      .connect(accounts[0])
+      .transfer(signerSmartAccount.address, ethers.utils.parseEther("100"));
+
+    await token
+      .connect(accounts[0])
+      .transfer(signerSmartAccount2.address, ethers.utils.parseEther("100"));
+
+    await token
+      .connect(accounts[0])
+      .transfer(mainSmartAccount2.address, ethers.utils.parseEther("100"));
+
+    expect(await token.balanceOf(charlie)).to.equal(0);
+
+    let tokensToBeTransferred = ethers.utils.parseEther("10");
+
+    // TX TO TRANSFER 10 tokens FROM mainSmartAccount to Charlie 
+    const safeTx: SafeTransaction = buildSafeTransaction({
+      to: token.address,
+      data: encodeTransfer(charlie, tokensToBeTransferred.toString()),
+      nonce: await mainSmartAccount.getNonce(EOA_CONTROLLED_FLOW),
+    });
+
+    const transaction: Transaction = {
+      to: safeTx.to,
+      value: safeTx.value,
+      data: safeTx.data,
+      operation: safeTx.operation,
+      targetTxGas: safeTx.targetTxGas,
+    };
+    const refundInfo: FeeRefund = {
+      baseGas: safeTx.baseGas,
+      gasPrice: safeTx.gasPrice,
+      tokenGasPriceFactor: safeTx.tokenGasPriceFactor,
+      gasToken: safeTx.gasToken,
+      refundReceiver: safeTx.refundReceiver,
+    };
+
+    const chainId = await mainSmartAccount.getChainId();
+
+    // BUILD 1271 SIGNATURE BY SIGNER SMART ACCOUNT
+
+    const { signer, data } = await safeSignTypedData(
+      accounts[0], //owner
+      mainSmartAccount,
+      safeTx,
+      chainId
+    );
+    
+    let signature = buildContractSignature(signerSmartAccount.address, data);
+
+    // MANIPULATE SIGNATURE TO SET signerContract2 as verifier
+    let addressToInsert = signerSmartAccount2.address.slice(2);
+
+    let manipulatedSignature = signature.replace(signature.substring(26,66), addressToInsert);
+
+    // Expect can not use this signature on main smart account 2, even despite 
+    // it is owned by signer smart account 2, that is owned by the owner (original signer)
+    await expect(
+      mainSmartAccount2.connect(accounts[1]).execTransaction(
+        transaction,
+        refundInfo,
+        manipulatedSignature
+      )
+    ).to.be.revertedWith("BSA024");
+
+    expect(await token.balanceOf(charlie)).to.equal(0);
+
+  });
 
   
+it("0x exploit 1271 | Reverts if trying to use 1271 signature instead of EOA signature", async function () { 
+  // i.e. trying to call isVslidSignature from EOA won't return magic value
+  // See https://samczsun.com/the-0x-vulnerability-explained/
+  await token
+  .connect(accounts[0])
+  .transfer(signerSmartAccount.address, ethers.utils.parseEther("100"));
+
+  let tokensToBeTransferred = ethers.utils.parseEther("10");
+
+  const safeTx: SafeTransaction = buildSafeTransaction({
+    to: token.address,
+    // value: ethers.utils.parseEther("1"),
+    data: encodeTransfer(charlie, tokensToBeTransferred.toString()),
+    nonce: await mainSmartAccount.getNonce(EOA_CONTROLLED_FLOW),
+  });
+
+  const transaction: Transaction = {
+    to: safeTx.to,
+    value: safeTx.value,
+    data: safeTx.data,
+    operation: safeTx.operation,
+    targetTxGas: safeTx.targetTxGas,
+  };
+  const refundInfo: FeeRefund = {
+    baseGas: safeTx.baseGas,
+    gasPrice: safeTx.gasPrice,
+    tokenGasPriceFactor: safeTx.tokenGasPriceFactor,
+    gasToken: safeTx.gasToken,
+    refundReceiver: safeTx.refundReceiver,
+  };
+
+  const chainId = await mainSmartAccount.getChainId();
+
+  // BUILD 1271 SIGNATURE BY OWNER
+
+  const { signer, data } = await safeSignTypedData(
+    accounts[0], //owner
+    mainSmartAccount,
+    safeTx,
+    chainId
+  );
+
+  let fakeSignature = buildContractSignature(accounts[0].address, data);
+
+  await expect (signerSmartAccount.connect(accounts[1]).execTransaction(
+    transaction,
+    refundInfo,
+    fakeSignature
+  )).to.be.reverted;
+  
+});
+
+it("0x exploit 1271 | Reverts if isValidSignature changes the state", async function () { 
+
+  let deployWalletIndex = 0;
+
+  const source = `
+            contract Test {
+                bool public changeState;
+                uint256 public nonce;
+                function isValidSignature(bytes32 _dataHash, bytes memory _signature) public returns (bytes4) {
+                    if (changeState) {
+                        nonce = nonce + 1;
+                    }
+                    return 0x1626ba7e;
+                }
+    
+                function shouldChangeState(bool value) public {
+                    changeState = value;
+                }
+            }`;
+    const testValidator = await deployContract(accounts[0], source);
+    await testValidator.shouldChangeState(true);
+
+    // Deploy Main Smart Account 2 owned by testValidator
+    let mainSmartAccount2Address = await walletFactory.getAddressForCounterfactualWallet(testValidator.address, deployWalletIndex);
+    
+    await walletFactory.deployCounterFactualWallet(
+      testValidator.address,
+      deployWalletIndex
+    );
+
+    let mainSmartAccount2 = await ethers.getContractAt(
+      "contracts/smart-contract-wallet/SmartAccount.sol:SmartAccount",
+      mainSmartAccount2Address
+    );
+
+    await token
+      .connect(accounts[0])
+      .transfer(mainSmartAccount2.address, ethers.utils.parseEther("100"));
+
+    expect(await token.balanceOf(charlie)).to.equal(0);
+
+    let tokensToBeTransferred = ethers.utils.parseEther("10");
+
+    const safeTx: SafeTransaction = buildSafeTransaction({
+      to: token.address,
+      data: encodeTransfer(charlie, tokensToBeTransferred.toString()),
+      nonce: await mainSmartAccount2.getNonce(EOA_CONTROLLED_FLOW),
+    });
+
+    const transaction: Transaction = {
+      to: safeTx.to,
+      value: safeTx.value,
+      data: safeTx.data,
+      operation: safeTx.operation,
+      targetTxGas: safeTx.targetTxGas,
+    };
+    const refundInfo: FeeRefund = {
+      baseGas: safeTx.baseGas,
+      gasPrice: safeTx.gasPrice,
+      tokenGasPriceFactor: safeTx.tokenGasPriceFactor,
+      gasToken: safeTx.gasToken,
+      refundReceiver: safeTx.refundReceiver,
+    };
+
+    const chainId = await mainSmartAccount2.getChainId();
+
+    // BUILD 1271 SIGNATURE BY test validator
+
+    const { signer, data } = await safeSignTypedData(
+      accounts[0], //owner
+      mainSmartAccount2,
+      safeTx,
+      chainId
+    );
+    
+    let signature = buildContractSignature(testValidator.address, data);
+
+    await expect (mainSmartAccount2.connect(accounts[1]).execTransaction(
+      transaction,
+      refundInfo,
+      signature
+    )).to.be.reverted;
+    expect(await token.balanceOf(charlie)).to.equal(0);
+
+    await testValidator.shouldChangeState(false);
+
+    await expect(
+      mainSmartAccount2.connect(accounts[1]).execTransaction(
+        transaction,
+        refundInfo,
+        signature
+      )
+    ).to.emit(mainSmartAccount2, "ExecutionSuccess");
+
+    expect(await token.balanceOf(charlie)).to.equal(
+      tokensToBeTransferred
+    );
+});
+
 });
