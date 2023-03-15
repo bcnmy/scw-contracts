ee80179f977a2f28c30f3a9c2333f86fda58d3f8
tomarsachin2271 <sachin.arox@gmail.com>
2023-03-16 00:54:33 +0400

    BSDK-409 Refactor Test cases to make it modular

--- test/paymaster/singleton/paymaster-accounting.ts
+++ test/paymaster/singleton/paymaster-accounting.ts
@@ -36,7 +36,199 @@ import { arrayify, formatEther, hexConcat, parseEther } from "ethers/lib/utils";
 import { BigNumber, Signer } from "ethers";
 import { UserOperation } from "../../utils/userOpetation";
 
-export async function deployEntryPoint(
+describe("Upgrade functionality Via Entrypoint", function () {
+  let entryPoint: EntryPoint;
+  let latestEntryPoint: EntryPoint;
+  let walletOwner: Signer;
+  let paymasterAddress: string;
+  let paymasterOffchainSigner: Signer, deployer: Signer;
+  let bundler: Signer;
+  let paymaster: VerifyingSingletonPaymaster;
+  let baseImpl: SmartWallet;
+  let walletFactory: WalletFactory;
+  let token: MockToken;
+  let multiSend: MultiSend;
+  let storage: StorageSetter;
+  let ownerAddress: string;
+  let deployerAddres: string;
+  let bobAddress: string;
+  let charlieAddress: string;
+  let paymasterId: string;
+  let userSCW: any;
+  let accounts: any;
+
+  before(async () => {
+    accounts = await ethers.getSigners();
+    entryPoint = await deployEntryPoint();
+
+    deployer = accounts[0];
+    paymasterOffchainSigner = accounts[1];
+    walletOwner = accounts[2];
+
+    deployerAddres = await deployer.getAddress();
+    ownerAddress = await walletOwner.getAddress();
+    bobAddress = await accounts[3].getAddress();
+    charlieAddress = await accounts[4].getAddress();
+    paymasterId = await accounts[5].getAddress();
+
+    bundler = accounts[6];
+    const offchainSignerAddress = await paymasterOffchainSigner.getAddress();
+
+    paymaster = await new VerifyingSingletonPaymaster__factory(deployer).deploy(
+      await deployer.getAddress(),
+      entryPoint.address,
+      offchainSignerAddress
+    );
+
+    const BaseImplementation = await ethers.getContractFactory("SmartAccount");
+    baseImpl = await BaseImplementation.deploy(entryPoint.address);
+    await baseImpl.deployed();
+
+    const WalletFactory = await ethers.getContractFactory(
+      "SmartAccountFactory"
+    );
+    walletFactory = await WalletFactory.deploy(baseImpl.address);
+    await walletFactory.deployed();
+
+    const MockToken = await ethers.getContractFactory("MockToken");
+    token = await MockToken.deploy();
+    await token.deployed();
+
+    const Storage = await ethers.getContractFactory("StorageSetter");
+    storage = await Storage.deploy();
+
+    const MultiSend = await ethers.getContractFactory("MultiSend");
+    multiSend = await MultiSend.deploy();
+
+    await token.mint(ownerAddress, ethers.utils.parseEther("1000000"));
+
+    paymasterAddress = paymaster.address;
+
+    await paymaster.connect(deployer).addStake(10, { value: parseEther("2") });
+    console.log("Paymaster staked");
+
+    await paymaster.depositFor(paymasterId, {
+      value: parseEther("1"),
+    });
+  });
+
+  describe("Check Paymaster Accounting is Correct", function () {
+    it("Ensure bundler is paid enough fee", async () => {
+      // Get User Op Object with initCode and paymaster data
+      const valueToTransfer = "1";
+      const callData = "0x";
+      const destinationAddress = charlieAddress;
+
+      const txnData = await getExecuteCallData(
+        destinationAddress,
+        valueToTransfer,
+        callData
+      );
+      const { expectedWalletAddress, userOp } =
+        await getUserOpWithInitCodeAndPaymasterData(
+          entryPoint,
+          walletFactory,
+          paymaster,
+          paymasterOffchainSigner,
+          paymasterId,
+          txnData,
+          walletOwner
+        );
+
+      // Send some funds to smart contract wallet
+      await sendEther(accounts[3], expectedWalletAddress, "5");
+
+      // Check paymaster deposit and paymasterID deposit before executing userOp
+      const paymasterDepositBefore = (
+        await entryPoint.getDepositInfo(paymasterAddress)
+      ).deposit;
+      const paymasterIdDepositBefore = await paymaster.getBalance(paymasterId);
+
+      // Execute UserOp transaction
+      const tx = await entryPoint
+        .connect(bundler)
+        .handleOps([userOp], await bundler.getAddress());
+
+      // Check paymaster deposit and paymasterID deposit after executing userOp
+      const paymasterDepositAfter = (
+        await entryPoint.getDepositInfo(paymasterAddress)
+      ).deposit;
+      const paymasterIdDepositAfter = await paymaster.getBalance(paymasterId);
+
+      // Get Fee paid by paymaster and paymasterID
+      const feePaidByPaymasterDeposit = paymasterDepositBefore.sub(
+        paymasterDepositAfter
+      );
+      const feePaidByPaymasterID = paymasterIdDepositBefore.sub(
+        paymasterIdDepositAfter
+      );
+
+      // Get actual transaction fee paid by bundler
+      const transactionFee = await getTransactionFee(tx);
+
+      await expect(feePaidByPaymasterID.toNumber()).to.be.greaterThan(
+        feePaidByPaymasterDeposit.toNumber()
+      );
+
+      // Ensure that tx fee paid by bundler is less than the refund paid by paymaster
+      await expect(transactionFee.toNumber()).to.be.lessThan(
+        feePaidByPaymasterDeposit.toNumber()
+      );
+    });
+  });
+});
+
+async function getUserOpWithInitCodeAndPaymasterData(
+  entryPoint: EntryPoint,
+  walletFactory: WalletFactory,
+  paymaster: VerifyingSingletonPaymaster,
+  paymasterOffchainSigner: Signer,
+  paymasterId: string,
+  callData: string,
+  walletOwner: Signer
+) {
+  const _walletOwnerAddress = await walletOwner.getAddress();
+  const paymasterAddress = await paymaster.address;
+  const initCode = await getWalletInitCode(
+    walletFactory,
+    _walletOwnerAddress,
+    0
+  );
+
+  const expectedWalletAddress = await getCounterFactualAddress(
+    walletFactory,
+    _walletOwnerAddress,
+    0
+  );
+
+  // Start filling the userOp
+  const userOp1 = await fillAndSign(
+    {
+      sender: expectedWalletAddress,
+      callData: callData,
+      verificationGasLimit: 320000,
+      initCode: initCode,
+    },
+    walletOwner,
+    entryPoint,
+    21000
+  );
+
+  // Set paymaster data in UserOp
+  const userOp = await getUserOpWithPaymasterData(
+    paymaster,
+    expectedWalletAddress,
+    userOp1,
+    paymasterOffchainSigner,
+    paymasterId,
+    paymasterAddress,
+    walletOwner,
+    entryPoint
+  );
+  return { expectedWalletAddress, userOp };
+}
+
+async function deployEntryPoint(
   provider = ethers.provider
 ): Promise<EntryPoint> {
   const epf = await (await ethers.getContractFactory("EntryPoint")).deploy();
@@ -81,216 +273,54 @@ async function getUserOpWithPaymasterData(
   return userOpWithPaymasterData;
 }
 
-describe("Upgrade functionality Via Entrypoint", function () {
-  let entryPoint: EntryPoint;
-  let latestEntryPoint: EntryPoint;
-  let walletOwner: Signer;
-  let paymasterAddress: string;
-  let offchainSigner: Signer, deployer: Signer;
-  let paymaster: VerifyingSingletonPaymaster;
-  let baseImpl: SmartWallet;
-  let walletFactory: WalletFactory;
-  let token: MockToken;
-  let multiSend: MultiSend;
-  let storage: StorageSetter;
-  let ownerAddress: string;
-  let deployerAddres: string;
-  let bobAddress: string;
-  let charlieAddress: string;
-  let paymasterId: string;
-  let userSCW: any;
-  let accounts: any;
+async function getWalletInitCode(
+  walletFactory: WalletFactory,
+  ownerAddress: string,
+  index: number
+) {
+  const WalletFactory = await ethers.getContractFactory("SmartAccountFactory");
 
-  before(async () => {
-    accounts = await ethers.getSigners();
-    entryPoint = await deployEntryPoint();
+  const encodedData = WalletFactory.interface.encodeFunctionData(
+    "deployCounterFactualAccount",
+    [ownerAddress, index]
+  );
+  return hexConcat([walletFactory.address, encodedData]);
+}
 
-    deployer = accounts[0];
-    offchainSigner = accounts[1];
-    walletOwner = accounts[2];
+async function getTransactionFee(tx: any) {
+  const receipt: any = await tx.wait();
+  return receipt.gasUsed.mul(receipt.effectiveGasPrice);
+}
 
-    deployerAddres = await deployer.getAddress();
-    ownerAddress = await walletOwner.getAddress();
-    bobAddress = await accounts[3].getAddress();
-    charlieAddress = await accounts[4].getAddress();
-    paymasterId = await accounts[5].getAddress();
+async function getExecuteCallData(
+  destination: string,
+  ethValue: string,
+  callData: string
+) {
+  const SmartAccount = await ethers.getContractFactory("SmartAccount");
+  const txnData = SmartAccount.interface.encodeFunctionData("executeCall", [
+    destination,
+    ethers.utils.parseEther(ethValue),
+    callData,
+  ]);
+  return txnData;
+}
 
-    const offchainSignerAddress = await offchainSigner.getAddress();
-
-    paymaster = await new VerifyingSingletonPaymaster__factory(deployer).deploy(
-      await deployer.getAddress(),
-      entryPoint.address,
-      offchainSignerAddress
-    );
-
-    const BaseImplementation = await ethers.getContractFactory("SmartAccount");
-    baseImpl = await BaseImplementation.deploy(entryPoint.address);
-    await baseImpl.deployed();
-
-    const WalletFactory = await ethers.getContractFactory(
-      "SmartAccountFactory"
-    );
-    walletFactory = await WalletFactory.deploy(baseImpl.address);
-    await walletFactory.deployed();
-
-    const MockToken = await ethers.getContractFactory("MockToken");
-    token = await MockToken.deploy();
-    await token.deployed();
-
-    const Storage = await ethers.getContractFactory("StorageSetter");
-    storage = await Storage.deploy();
-
-    const MultiSend = await ethers.getContractFactory("MultiSend");
-    multiSend = await MultiSend.deploy();
-
-    await token.mint(ownerAddress, ethers.utils.parseEther("1000000"));
-
-    paymasterAddress = paymaster.address;
-
-    await paymaster.connect(deployer).addStake(10, { value: parseEther("2") });
-    console.log("Paymaster staked");
-
-    await paymaster.depositFor(paymasterId, {
-      value: parseEther("1"),
-    });
+async function sendEther(from: any, to: string, amount: string) {
+  await from.sendTransaction({
+    from: await from.getAddress(),
+    to: to,
+    value: ethers.utils.parseEther(amount),
   });
+}
 
-  describe("Basic Userops using Entrypoint", function () {
-    it("succeed with valid signature", async () => {
-      // Deploying wallet first
-      await walletFactory.deployCounterFactualAccount(ownerAddress, 0);
-      const expectedSmartAccountAddress =
-        await walletFactory.getAddressForCounterfactualAccount(ownerAddress, 0);
-
-      // Send some funds to smart contract wallet
-      await accounts[3].sendTransaction({
-        from: bobAddress,
-        to: expectedSmartAccountAddress,
-        value: ethers.utils.parseEther("5"),
-      });
-
-      // Prepare CallData for userOp
-      const SmartAccount = await ethers.getContractFactory("SmartAccount");
-      const txnData = SmartAccount.interface.encodeFunctionData("executeCall", [
-        charlieAddress,
-        ethers.utils.parseEther("1"),
-        "0x",
-      ]);
-
-      // Start filling the userOp
-      const userOp1 = await fillAndSign(
-        {
-          sender: expectedSmartAccountAddress,
-          callData: txnData,
-          verificationGasLimit: 200000,
-        },
-        walletOwner,
-        entryPoint,
-        22288 + 3422
-      );
-
-      // Set paymaster data in UserOp
-      const userOp = await getUserOpWithPaymasterData(
-        paymaster,
-        expectedSmartAccountAddress,
-        userOp1,
-        offchainSigner,
-        paymasterId,
-        paymasterAddress,
-        walletOwner,
-        entryPoint
-      );
-      userOp.preVerificationGas = Number(userOp.preVerificationGas);
-      const paymasterDepositBefore = await entryPoint.getDepositInfo(
-        paymasterAddress
-      );
-      //   console.log(
-      //     "Paymaster deposit before tx: ",
-      //     formatEther(paymasterDepositBefore.deposit)
-      //   );
-
-      const paymasterIdDepositBefore = await paymaster.getBalance(paymasterId);
-      //   console.log(
-      //     "PaymasterId Deposit Before: ",
-      //     formatEther(paymasterIdDepositBefore)
-      //   );
-
-      const relayerBalanceBefore = await ethers.provider.getBalance(
-        deployerAddres
-      );
-      console.log(
-        "Balance of relayer before tx: ",
-        formatEther(relayerBalanceBefore)
-      );
-      console.log(userOp);
-      // console.log(
-      //   "MaxFeePerGas",
-      //   userOp.maxFeePerGas,
-      //   " MaxPriorityFeePerGas: ",
-      //   userOp.maxPriorityFeePerGas
-      // );
-      // Execute UserOp transaction
-      const tx = await entryPoint
-        .connect(deployer)
-        .handleOps([userOp], deployerAddres, {
-          maxPriorityFeePerGas: "1000000000",
-          maxFeePerGas: "1222585661",
-        });
-
-      // Get Paymster deposit information on entry point
-      const paymasterDepositAfter = await entryPoint.getDepositInfo(
-        paymasterAddress
-      );
-      // console.log(
-      //   "Paymaster deposit after tx: ",
-      //   formatEther(paymasterDepositAfter.deposit)
-      // );
-
-      const paymasterIdDepositAfter = await paymaster.getBalance(paymasterId);
-      // console.log(
-      //   "PaymasterId Deposit After: ",
-      //   formatEther(paymasterIdDepositAfter)
-      // );
-
-      // Get Receipt and Transaction Fee
-      const receipt: any = await tx.wait();
-      // console.log("Gas Used: ", receipt.gasUsed);
-      console.log(
-        "************ Effective Gas Price: ",
-        receipt.effectiveGasPrice
-      );
-      // console.log("Block Number: ", tx.blockNumber);
-      const feeData = ethers.provider.getFeeData();
-      // console.log("Base Fee: ", (await feeData).lastBaseFeePerGas);
-
-      const transactionFee = receipt.gasUsed.mul(receipt.effectiveGasPrice);
-
-      const relayerBalanceAfter = await ethers.provider.getBalance(
-        deployerAddres
-      );
-      console.log(
-        "Balance of relayer after tx: ",
-        formatEther(relayerBalanceAfter)
-      );
-      console.log(
-        "Difference in relayer balance (After - Before) (Should be +ve): ",
-        formatEther(relayerBalanceAfter.sub(relayerBalanceBefore))
-      );
-      console.log("Transaction Fee: ", formatEther(transactionFee));
-
-      console.log(
-        "Paymaster deposit deducted from entry Point: ",
-        formatEther(
-          paymasterDepositBefore.deposit.sub(paymasterDepositAfter.deposit)
-        )
-      );
-      console.log(
-        "PaymasterID deposit deducted from Paymaster: ",
-        formatEther(paymasterIdDepositBefore.sub(paymasterIdDepositAfter))
-      );
-      await expect(
-        entryPoint.handleOps([userOp], await offchainSigner.getAddress())
-      ).to.be.reverted;
-    });
-  });
-});
+async function getCounterFactualAddress(
+  walletFactory: WalletFactory,
+  ownerAddress: string,
+  index: number
+) {
+  return await walletFactory.getAddressForCounterfactualAccount(
+    ownerAddress,
+    index
+  );
+}
