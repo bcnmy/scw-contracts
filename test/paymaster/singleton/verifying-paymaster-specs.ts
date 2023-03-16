05f5a991ea139f4b678ac872948e7f52ac318791
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-03-16 16:11:58 +0400

    update test case

--- test/paymaster/singleton/verifying-paymaster-specs.ts
+++ test/paymaster/singleton/verifying-paymaster-specs.ts
@@ -4,22 +4,18 @@ import { ethers } from "hardhat";
 import {
   SmartAccount,
   SmartAccount__factory,
-  DefaultCallbackHandler,
-  DefaultCallbackHandler__factory,
   EntryPoint,
   VerifyingSingletonPaymaster,
   VerifyingSingletonPaymaster__factory,
   SmartAccountFactory,
   SmartAccountFactory__factory,
-  MaliciousAccount2,
   MaliciousAccount2__factory,
   EntryPoint__factory,
 } from "../../../typechain";
 import { AddressZero } from "../../smart-wallet/testutils";
-import { simulationResultCatch } from "../../aa-core/testutils";
-import { fillAndSign } from "../../utils/userOp";
-import { arrayify, hexConcat, parseEther } from "ethers/lib/utils";
-import { Contract, Signer } from "ethers";
+// import { fillAndSign } from "../../utils/userOp";
+import { parseEther } from "ethers/lib/utils";
+import { Signer } from "ethers";
 
 export async function deployEntryPoint(
   provider = ethers.provider
@@ -38,10 +34,8 @@ describe("EntryPoint with VerifyingPaymaster Singleton", function () {
   let offchainSigner: Signer, deployer: Signer;
 
   let verifyingSingletonPaymaster: VerifyingSingletonPaymaster;
-  let smartWalletImp: SmartWallet;
-  let maliciousWallet: MaliciousAccount2;
-  let walletFactory: WalletFactory;
-  const abi = ethers.utils.defaultAbiCoder;
+  let smartWalletImp: SmartAccount;
+  let walletFactory: SmartAccountFactory;
 
   before(async function () {
     accounts = await ethers.getSigners();
@@ -66,10 +60,6 @@ describe("EntryPoint with VerifyingPaymaster Singleton", function () {
       entryPoint.address
     );
 
-    maliciousWallet = await new MaliciousAccount2__factory(deployer).deploy(
-      entryPoint.address
-    );
-
     walletFactory = await new SmartAccountFactory__factory(deployer).deploy(
       smartWalletImp.address
     );
@@ -86,7 +76,7 @@ describe("EntryPoint with VerifyingPaymaster Singleton", function () {
     paymasterAddress = verifyingSingletonPaymaster.address;
     console.log("Paymaster address is ", paymasterAddress);
 
-    await entryPoint.depositTo(paymasterAddress, { value: parseEther("1") });
+    await entryPoint.depositTo(paymasterAddress, { value: parseEther("2") });
   });
 
   describe("getBalance: get the current deposit for paymasterId", () => {
@@ -97,51 +87,55 @@ describe("EntryPoint with VerifyingPaymaster Singleton", function () {
     });
 
     it("getBalance: returns deposit of paymaster", async () => {
-      const paymasterId = await offchainSigner.getAddress();
+      const paymasterId = await depositorSigner.getAddress();
       await verifyingSingletonPaymaster.depositFor(paymasterId, {
         value: parseEther("1"),
       });
       const bal = await verifyingSingletonPaymaster.getBalance(paymasterId);
       expect(bal).to.be.equal(parseEther("1"));
     });
-  });
 
-  it("deposit: should revert this", async () => {
-    const tx = verifyingSingletonPaymaster.deposit({
-      value: ethers.utils.parseEther("1"),
+    it("deposit: should revert this", async () => {
+      const tx = verifyingSingletonPaymaster.deposit({
+        value: ethers.utils.parseEther("1"),
+      });
+      await expect(tx).to.be.revertedWith("user DepositFor instead");
     });
-    await expect(tx).to.be.revertedWith("user DepositFor instead");
-  });
 
-  it("withdrawTo: should be able to withdraw to any address", async () => {
-    const paymasterId = await offchainSigner.getAddress();
-    const balBefore = await ethers.provider.getBalance(paymasterId);
-    console.log("balBefore", balBefore.toString());
-    const tx = verifyingSingletonPaymaster
-      .connect(deployer)
-      .withdrawTo(AddressZero, parseEther("0.5"));
-    expect(tx).to.be.revertedWith("CanNotWithdrawToZeroAddress");
+    it("withdrawTo: should be able to withdraw to any address", async () => {
+      const paymasterId = await depositorSigner.getAddress();
+      const paymasterIdDepositBefore =
+        await verifyingSingletonPaymaster.getBalance(paymasterId);
+      const tx = verifyingSingletonPaymaster
+        .connect(deployer)
+        .withdrawTo(AddressZero, parseEther("0.5"));
+      expect(tx).to.be.revertedWith("CanNotWithdrawToZeroAddress");
 
-    // await verifyingSingletonPaymaster
-    //   .connect(deployer)
-    //   .withdrawTo(paymasterId, parseEther("0.5"));
-    // const balAfter = await ethers.provider.getBalance(paymasterId);
-    // console.log("balAfter", balAfter.toString());
-    // expect(balAfter.sub(balBefore)).to.be.equal(parseEther("0"));
-  });
+      await verifyingSingletonPaymaster
+        .connect(depositorSigner)
+        .withdrawTo(paymasterId, parseEther("0.5"));
 
-  it("setSigner: should be able to set signer", async () => {
-    const tx1 = verifyingSingletonPaymaster
-      .connect(deployer)
-      .setSigner(AddressZero);
-    expect(tx1).to.be.revertedWith("VerifyingSignerCannotBeZero");
+      const paymasterIdDepositAfter =
+        await verifyingSingletonPaymaster.getBalance(paymasterId);
 
-    const newSigner = await accounts[5].address;
-    const tx2 = await verifyingSingletonPaymaster
-      .connect(deployer)
-      .setSigner(newSigner);
-    expect(tx2)
-      .to.emit(verifyingSingletonPaymaster, "SignerChanged")
-      .withArgs(newSigner);
+      expect(paymasterIdDepositBefore.sub(paymasterIdDepositAfter)).to.be.equal(
+        parseEther("0.5")
+      );
+    });
+
+    it("setSigner: should be able to set signer", async () => {
+      const tx1 = verifyingSingletonPaymaster
+        .connect(deployer)
+        .setSigner(AddressZero);
+      expect(tx1).to.be.revertedWith("VerifyingSignerCannotBeZero");
+
+      const newSigner = await accounts[5].address;
+      const tx2 = await verifyingSingletonPaymaster
+        .connect(deployer)
+        .setSigner(newSigner);
+      expect(tx2)
+        .to.emit(verifyingSingletonPaymaster, "SignerChanged")
+        .withArgs(newSigner);
+    });
   });
 });
