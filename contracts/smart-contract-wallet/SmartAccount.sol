773507a0c0cb187c6c1a1ba50fbf44687be8904c
Filipp Makarov <filmakarov@filipps-mbp-2.t-com.me>
2023-05-11 12:33:40 +0200

    remove old upgrade test case

--- contracts/smart-contract-wallet/SmartAccount.sol
+++ contracts/smart-contract-wallet/SmartAccount.sol
@@ -52,7 +52,7 @@ contract SmartAccount is
         0xda033865d68bf4a40a5a7cb4159a99e33dba8569e65ea3e38222eb12d9e66eee;
 
     // Owner storage. Deprecated. Left for storage layout compatibility
-    address public deprecatedOwner;
+    address public owner_deprecated;
 
     // changed to 2D nonce below
     // @notice there is no _nonce
