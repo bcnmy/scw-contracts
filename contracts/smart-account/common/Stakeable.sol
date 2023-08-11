be214a420df06943391cfef2069a480d8f10dfbd
Filipp Makarov <filmakarov@Filipps-MacBook-Pro-2.local>
2023-08-11 14:38:44 +0300

    stakeable factory

---
+++ contracts/smart-account/common/Stakeable.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+/**
+ * @title Stakeable Entity
+ * @author Fil Makarov - <filipp.makarov@biconomy.io>
+ */
+contract Stakeable is Ownable {
+    constructor(address _newOwner) {
+        _transferOwnership(_newOwner);
+    }
+
+    function addStake(
+        address epAddress,
+        uint32 unstakeDelaySec
+    ) external payable onlyOwner {
+        IEntryPoint(epAddress).addStake{value: msg.value}(unstakeDelaySec);
+    }
+
+    function unlockStake(address epAddress) external onlyOwner {
+        IEntryPoint(epAddress).unlockStake();
+    }
+
+    function withdrawStake(
+        address epAddress,
+        address payable withdrawAddress
+    ) external onlyOwner {
+        IEntryPoint(epAddress).withdrawStake(withdrawAddress);
+    }
+}
--- test/Smart-Account-Factory.specs.ts
+++ test/Smart-Account-Factory.specs.ts
@@ -32,14 +32,14 @@ describe("Smart Account Factory", async () => {
     it ("reverts with zero address provided as implementation", async () => {
       const SmartAccountFactory = await ethers.getContractFactory("SmartAccountFactory");
       await expect(
-        SmartAccountFactory.deploy(AddressZero)
+        SmartAccountFactory.deploy(AddressZero, deployer.address)
       ).to.be.revertedWith("implementation cannot be zero");
     });
 
     it ("sets non-zero address implementation", async () => {
       const { smartAccountImplementation } = await setupTests();
       const SmartAccountFactory = await ethers.getContractFactory("SmartAccountFactory");
-      const testFactory = await SmartAccountFactory.deploy(smartAccountImplementation.address);
+      const testFactory = await SmartAccountFactory.deploy(smartAccountImplementation.address, deployer.address);
       await testFactory.deployed();
       expect(await testFactory.basicImplementation()).to.equal(smartAccountImplementation.address);
     });
@@ -47,13 +47,21 @@ describe("Smart Account Factory", async () => {
     it ("deploys Default Callback Handler instance", async () => {
       const { smartAccountImplementation } = await setupTests();
       const SmartAccountFactory = await ethers.getContractFactory("SmartAccountFactory");
-      const testFactory = await SmartAccountFactory.deploy(smartAccountImplementation.address);
+      const testFactory = await SmartAccountFactory.deploy(smartAccountImplementation.address, deployer.address);
       await testFactory.deployed();
       const callbackHandlerAddress = await testFactory.minimalHandler();
       const callbackHandler = await ethers.getContractAt("DefaultCallbackHandler", callbackHandlerAddress);
       expect(await callbackHandler.NAME()).to.equal("Default Callback Handler");
     });
 
+    it ("successfully changes owner to a new one", async () => {
+      const { smartAccountImplementation } = await setupTests();
+      const SmartAccountFactory = await ethers.getContractFactory("SmartAccountFactory");
+      const testFactory = await SmartAccountFactory.deploy(smartAccountImplementation.address, charlie.address);
+      await testFactory.deployed();
+      expect(await testFactory.owner()).to.equal(charlie.address);
+      expect(await testFactory.owner()).to.not.equal(deployer.address);
+    });
   });
 
   describe("Deploy CounterFactual Account", async () => { 
