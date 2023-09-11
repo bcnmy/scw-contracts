5de64daa872d60c9f78fda1ccfeee47f5a65d05a
aboudjem <boudjemaa.adam@gmail.com>
2023-09-11 21:53:53 +0700

    🚨 reorder func, fix lint

--- contracts/smart-account/utils/GasEstimatorSmartAccount.sol
+++ contracts/smart-account/utils/GasEstimatorSmartAccount.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "../factory/SmartAccountFactory.sol";
+import {SmartAccountFactory} from "../factory/SmartAccountFactory.sol";
 
 // Contract for estimating gas on undeployed smart account
 // Deploys a smart account and then calls the appropriate method
@@ -14,9 +14,8 @@ contract GasEstimatorSmartAccount {
         uint256 _index,
         bytes calldata _data // execTransaction data // counterFactual wallet should have assets if required
     ) external returns (bool success, bytes memory result, uint256 gas) {
-        // solhint-disable
         uint256 initialGas = gasleft();
-        address _wallet = SmartAccountFactory(_factory)
+        SmartAccountFactory(_factory)
             .deployCounterFactualAccount(
                 _moduleSetupContract,
                 _moduleSetupData,
@@ -24,6 +23,5 @@ contract GasEstimatorSmartAccount {
             );
         (success, result) = _actualWallet.call(_data);
         gas = initialGas - gasleft();
-        // solhint-enable
     }
 }
