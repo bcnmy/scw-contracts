64a14f1fd13d72290162ec03c6759a09a91b4db3
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-10-11 16:44:32 +0400

    paymaster usage from paymasters package - if needed for test cases

--- contracts/smart-account/test/TestPaymaster.sol
+++ contracts/smart-account/test/TestPaymaster.sol
@@ -2,5 +2,5 @@
 pragma solidity 0.8.17;
 
 // Mock sol file to get the artifcats generated for this package
-import { VerifyingSingletonPaymaster } from "@biconomy-devx/paymasters/contracts/verifying/VerifyingSingletonPaymaster.sol";
-import { IEntryPoint } from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {VerifyingSingletonPaymaster} from "@biconomy-devx/paymasters/contracts/verifying/VerifyingSingletonPaymaster.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
