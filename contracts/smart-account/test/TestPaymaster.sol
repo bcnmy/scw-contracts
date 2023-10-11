57ee67649d2b78baf66d770c6e5579ce8532ecd3
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-10-11 16:53:22 +0400

    respond to PR comments

--- contracts/smart-account/test/TestPaymaster.sol
+++ contracts/smart-account/test/TestPaymaster.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-// Mock sol file to get the artifcats generated for this package
+// Mock sol file to get the artifacts generated for this package
 import {VerifyingSingletonPaymaster} from "@biconomy-devx/paymasters/contracts/verifying/VerifyingSingletonPaymaster.sol";
-import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
