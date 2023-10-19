72df6c8c8beb7e41e14ae94686db44396394a7d2
Ankur Dubey <iam@ankurdubey.com>
2023-10-19 19:21:31 +0530

    feat: ERC7484 Security Policy

--- contracts/smart-account/interfaces/modules/IERC7484SecurityPolicyPlugin.sol
+++ contracts/smart-account/interfaces/modules/IERC7484SecurityPolicyPlugin.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
 import {ISecurityPolicyPlugin} from "contracts/smart-account/interfaces/modules/ISecurityPolicyManagerPlugin.sol";
 
+/// @title ERC7484 Security Policy Plugin
+/// @author @ankurdubey521
+/// @dev Second Order Plugin to the Security Policy Manager Plugin, enforces checks as defined by ERC7484
+// https://github.com/ethereum/EIPs/blob/231f3e25889dae1c7d21b4419fa27cee79a4ca42/EIPS/eip-7484.mdcontract
 interface IERC7484SecurityPolicyPlugin is ISecurityPolicyPlugin {
-    struct PolicyDetails {
-        uint256 x;
+    struct Configuration {
+        address[] trustedAttesters;
+        uint256 threshold;
     }
+
+    event ConfigurationSet(address _sa, Configuration configuration);
+
+    /// @dev Sets the configuration for the plugin for a given account
+    /// @param _configuration The configuration to set
+    function setConfiguration(Configuration calldata _configuration) external;
+
+    /// @dev Gets the configuration for the plugin for a given account
+    /// @param _sa The account to get the configuration for
+    function configuration(
+        address _sa
+    ) external view returns (Configuration memory);
 }
--- contracts/smart-account/modules/SecurityPolicies/ERC7484SecurityPolicy.sol
+++ contracts/smart-account/modules/SecurityPolicies/ERC7484SecurityPolicy.sol
@@ -1,15 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
-// import {ISecurityPolicyPlugin} from "contracts/smart-account/interfaces/modules/ISecurityPolicyManagerPlugin.sol";
+import {IERC7484SecurityPolicyPlugin, ISecurityPolicyPlugin} from "contracts/smart-account/interfaces/modules/IERC7484SecurityPolicyPlugin.sol";
+import {IQuery} from "registry/src/interface/IQuery.sol";
 
 /// @title ERC7484 Security Policy Plugin
 /// @author @ankurdubey521
 /// @dev Second Order Plugin to the Security Policy Manager Plugin, enforces checks as defined by ERC7484
 // https://github.com/ethereum/EIPs/blob/231f3e25889dae1c7d21b4419fa27cee79a4ca42/EIPS/eip-7484.mdcontract
+contract ERC7484SecurityPolicyPlugin is IERC7484SecurityPolicyPlugin {
+    IQuery public immutable registry;
 
-contract ERC7484SecurityPolicyPlugin {
-    function foo() external pure returns (uint256) {
-        return 1;
+    mapping(address => Configuration) _configuration;
+
+    constructor(IQuery _regisry) {
+        registry = _regisry;
+    }
+
+    /// @inheritdoc ISecurityPolicyPlugin
+    function validateSecurityPolicy(
+        address _scw,
+        address _plugin
+    ) external override {
+        registry.checkN(
+            _plugin,
+            _configuration[_scw].trustedAttesters,
+            _configuration[_scw].threshold
+        );
+    }
+
+    /// @inheritdoc IERC7484SecurityPolicyPlugin
+    function setConfiguration(
+        Configuration calldata _config
+    ) external override {
+        _configuration[msg.sender] = _config;
+        emit ConfigurationSet(msg.sender, _config);
+    }
+
+    /// @inheritdoc IERC7484SecurityPolicyPlugin
+    function configuration(
+        address _sa
+    ) external view override returns (Configuration memory) {
+        return _configuration[_sa];
     }
 }
