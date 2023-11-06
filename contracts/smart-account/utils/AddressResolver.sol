ada05050da6430e4141af7c75be27ad1f21e698f
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-11-06 20:55:36 +0530

    update contract methods

--- contracts/smart-account/utils/AddressResolver.sol
+++ contracts/smart-account/utils/AddressResolver.sol
@@ -5,6 +5,7 @@ import {IEcdsaOwnershipRegistryModule} from "../interfaces/modules/IEcdsaOwnersh
 import {IAddressResolver} from "../interfaces/IAddressResolver.sol";
 import {ISmartAccount} from "../interfaces/ISmartAccount.sol";
 
+/// EOA <-> Smart Account address resolver for Biconomy smart accounts
 contract AddressResolver {
     struct SmartAccountResult {
         address accountAddress;
@@ -19,18 +20,17 @@ contract AddressResolver {
         0x000000F9eE1842Bb72F6BBDD75E6D3d4e3e9594C;
     address public constant SA_V2_FACTORY =
         0x000000a56Aaca3e9a4C479ea6b6CD0DbcB6634F5;
-    // marked for deletion
-    address public constant SA_V1_IMPLEMENTATION =
-        0x00006B7e42e01957dA540Dc6a8F7C30c4D816af5;
-    // marked for deletion
-    address public constant SA_V2_IMPLEMENTATION =
-        0x0000002512019Dafb59528B82CB92D3c5D2423aC;
     address public constant ECDSA_REGISTRY_MODULE_ADDRESS =
         0x0000001c5b32F37F5beA87BDD5374eB2aC54eA8e;
 
-    // Todo Add natspec
-    // Review could pass factory and module address in params
-    function resolveEOAtoAccountAddresses(
+    /**
+     * @dev Returns the addresses of all the smart accounts deployed by the EOA for any deployment index from 0 to _maxIndex.
+     * @param _eoa Address of the EOA.
+     * @param _maxIndex Maximum index to check.
+     * @notice This function is only for V1 and V2 Biconomy smart accounts.
+     * @notice For V2 smart accounts, the _moduleAddress and _moduleSetupData parameters are not used. It assumes ECDSA module.
+     */
+    function resolveAddresses(
         address _eoa,
         uint8 _maxIndex
     ) external view returns (SmartAccountResult[] memory) {
@@ -63,6 +63,70 @@ contract AddressResolver {
                 .getAddressForCounterFactualAccount(
                     ECDSA_REGISTRY_MODULE_ADDRESS,
                     data,
+                    i
+                );
+            if (v2Address != address(0) && _isSmartContract(v2Address)) {
+                _saInfo[saInfoIndex] = SmartAccountResult(
+                    v2Address,
+                    SA_V2_FACTORY,
+                    ISmartAccount(v2Address).getImplementation(),
+                    ISmartAccount(v2Address).VERSION(),
+                    "v2",
+                    i
+                );
+                saInfoIndex++;
+            }
+        }
+
+        // Create a new dynamic array with only the used elements
+        SmartAccountResult[] memory result = new SmartAccountResult[](
+            saInfoIndex
+        );
+        for (uint256 j = 0; j < saInfoIndex; j++) {
+            result[j] = _saInfo[j];
+        }
+        return result;
+    }
+
+    /**
+     * @dev Returns the addresses of all the smart accounts deployed by the EOA for any deployment index from 0 to _maxIndex.
+     * @param _eoa Address of the EOA.
+     * @param _maxIndex Maximum index to check.
+     * @param _moduleAddress Address of the auth module used to deploy the smart accounts.
+     * @param _moduleSetupData module setup data used to deploy the smart accounts.
+     * @notice This function is only for V1 and V2 Biconomy smart accounts.
+     * @notice For V2 smart accounts, the _moduleAddress and _moduleSetupData parameters are used. It can be any auth module (which uses ecda owner) used with the factory
+     */
+    function resolveAddressesFlexibleForV2(
+        address _eoa,
+        uint8 _maxIndex,
+        address _moduleAddress, // V2 factory could use any auth module
+        bytes memory _moduleSetupData
+    ) external view returns (SmartAccountResult[] memory) {
+        SmartAccountResult[] memory _saInfo = new SmartAccountResult[](
+            _maxIndex * 2
+        );
+        uint256 saInfoIndex = 0; // To keep track of the current index in _saAddresses
+
+        for (uint256 i; i < _maxIndex; i++) {
+            address v1Address = IAddressResolver(SA_V1_FACTORY)
+                .getAddressForCounterFactualAccount(_eoa, i);
+            if (v1Address != address(0) && _isSmartContract(v1Address)) {
+                _saInfo[saInfoIndex] = SmartAccountResult(
+                    v1Address,
+                    SA_V1_FACTORY,
+                    ISmartAccount(v1Address).getImplementation(),
+                    ISmartAccount(v1Address).VERSION(),
+                    "v1",
+                    i
+                );
+                saInfoIndex++;
+            }
+
+            address v2Address = IAddressResolver(SA_V2_FACTORY)
+                .getAddressForCounterFactualAccount(
+                    _moduleAddress,
+                    _moduleSetupData,
                     i
                 );
             if (v2Address != address(0) && _isSmartContract(v2Address)) {
