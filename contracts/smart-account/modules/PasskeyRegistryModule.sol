d869571da2f1592ee1325febe893a3a2d833e72e
amanraj1608 <archanaamanraj@gmail.com>
2023-11-29 23:13:15 +0530

    update PasskeyRegistryModule test cases

--- contracts/smart-account/modules/PasskeyRegistryModule.sol
+++ contracts/smart-account/modules/PasskeyRegistryModule.sol
@@ -25,7 +25,7 @@ contract PasskeyRegistryModule is
     string public constant NAME = "PassKeys Ownership Registry Module";
     string public constant VERSION = "0.2.0";
 
-    mapping(address => PassKeyId) public smartAccountPassKeys;
+    mapping(address => PassKeyId) public _smartAccountPasskey;
 
     /// @inheritdoc IPasskeyRegistryModule
     function initForSmartAccount(
@@ -33,18 +33,29 @@ contract PasskeyRegistryModule is
         uint256 _pubKeyY,
         string calldata _keyId
     ) external override returns (address) {
-        PassKeyId storage passKeyId = smartAccountPassKeys[msg.sender];
+        PassKeyId storage passKeyId = _smartAccountPasskey[msg.sender];
 
         if (passKeyId.pubKeyX != 0 && passKeyId.pubKeyY != 0)
             revert AlreadyInitedForSmartAccount(msg.sender);
 
-        smartAccountPassKeys[msg.sender] = PassKeyId(
+        _smartAccountPasskey[msg.sender] = PassKeyId(
             _pubKeyX,
             _pubKeyY,
             _keyId
         );
 
         return address(this);
+    }
+
+    /**
+     * @dev Returns the owner of the Smart Account. Reverts for Smart Accounts without owners.
+     * @param smartAccount Smart Account address.
+     * @return PassKeyId The owner key of the Smart Account.
+     */
+    function getOwner(
+        address smartAccount
+    ) external view returns (PassKeyId memory) {
+        return _smartAccountPasskey[smartAccount];
     }
 
     /// @inheritdoc IAuthorizationModule
@@ -56,49 +67,81 @@ contract PasskeyRegistryModule is
             userOp.signature,
             (bytes, address)
         );
-        if (_verifySignature(userOpHash, passkeySignature)) {
+        if (_verifySignature(userOpHash, passkeySignature, userOp.sender)) {
             return VALIDATION_SUCCESS;
         }
         return SIG_VALIDATION_FAILED;
     }
 
-    /// @inheritdoc ISignatureValidator
+    /**
+     * @inheritdoc ISignatureValidator
+     * @dev Validates a signature for a message.
+     * @dev Appends smart account address to the hash to avoid replay attacks
+     * To be called from a Smart Account.
+     * @param signedDataHash Hash of the message that was signed.
+     * @param moduleSignature Signature to be validated.
+     * @return EIP1271_MAGIC_VALUE if signature is valid, 0xffffffff otherwise.
+     */
     function isValidSignature(
         bytes32 signedDataHash,
         bytes memory moduleSignature
     ) public view virtual override returns (bytes4) {
-        // TODO: @amanraj1608 make it safe
-        return isValidSignatureForAddress(signedDataHash, moduleSignature);
+        return
+            isValidSignatureForAddress(
+                signedDataHash,
+                moduleSignature,
+                msg.sender
+            );
+    }
+
+    /// @inheritdoc IPasskeyRegistryModule
+    function isValidSignatureForAddress(
+        bytes32 signedDataHash,
+        bytes memory moduleSignature,
+        address smartAccount
+    ) public view virtual returns (bytes4) {
+        if (
+            _verifySignature(
+                keccak256(
+                    abi.encodePacked(
+                        "\x19Ethereum Signed Message:\n52",
+                        signedDataHash,
+                        smartAccount
+                    )
+                ),
+                moduleSignature,
+                smartAccount
+            )
+        ) {
+            return EIP1271_MAGIC_VALUE;
+        }
+        return bytes4(0xffffffff);
     }
 
     /// @inheritdoc ISignatureValidator
     function isValidSignatureUnsafe(
         bytes32 signedDataHash,
         bytes memory moduleSignature
-    ) public view virtual override returns (bytes4) {
-        return isValidSignatureForAddress(signedDataHash, moduleSignature);
-    }
-
-    /// @inheritdoc IPasskeyRegistryModule
-    function isValidSignatureForAddress(
-        bytes32 signedDataHash,
-        bytes memory moduleSignature
-    ) public view virtual override returns (bytes4) {
-        if (_verifySignature(signedDataHash, moduleSignature)) {
-            return EIP1271_MAGIC_VALUE;
-        }
-        return bytes4(0xffffffff);
+    ) public view virtual returns (bytes4) {
+        return
+            isValidSignatureForAddress(
+                signedDataHash,
+                moduleSignature,
+                msg.sender
+            );
     }
 
     /**
      * @dev Internal utility function to verify a signature.
      * @param userOpDataHash The hash of the user operation data.
      * @param moduleSignature The signature provided by the module.
+     * @param smartAccount The smart account address.
      * @return True if the signature is valid, false otherwise.
      */
     function _verifySignature(
         bytes32 userOpDataHash,
-        bytes memory moduleSignature
+        bytes memory moduleSignature,
+        address smartAccount
     ) internal view returns (bool) {
         (
             bytes32 keyHash,
@@ -123,26 +166,10 @@ contract PasskeyRegistryModule is
         bytes32 clientHash = sha256(bytes(clientDataJSON));
         bytes32 sigHash = sha256(bytes.concat(authenticatorData, clientHash));
 
-        PassKeyId memory passKey = smartAccountPassKeys[msg.sender];
+        PassKeyId memory passKey = _smartAccountPasskey[smartAccount];
         if (passKey.pubKeyX == 0 && passKey.pubKeyY == 0) {
-            revert NoPassKeyRegisteredForSmartAccount(msg.sender);
+            revert NoPassKeyRegisteredForSmartAccount(smartAccount);
         }
         return Secp256r1.verify(passKey, sigx, sigy, uint256(sigHash));
-    }
-
-    /**
-     * @dev Internal function to validate a user operation signature.
-     * @param userOp The user operation to validate.
-     * @param userOpHash The hash of the user operation.
-     * @return sigValidationResult Returns 0 if the signature is valid, and SIG_VALIDATION_FAILED otherwise.
-     */
-    function _validateSignature(
-        UserOperation calldata userOp,
-        bytes32 userOpHash
-    ) internal view virtual returns (uint256 sigValidationResult) {
-        if (_verifySignature(userOpHash, userOp.signature)) {
-            return 0;
-        }
-        return SIG_VALIDATION_FAILED;
     }
 }
