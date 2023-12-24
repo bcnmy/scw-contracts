878d38effa4a440ae0297e69d4cfd725545901d9
Ankur Dubey <iam@ankurdubey.com>
2023-12-25 03:10:59 +0530

    fix: Restore Session Key Manager Module and Batched Session Router

--- contracts/smart-account/modules/SessionKeyManagers/SessionKeyManagerModule.sol
+++ contracts/smart-account/modules/SessionKeyManagers/SessionKeyManagerModule.sol
@@ -113,7 +113,7 @@ contract SessionKeyManager is
         if (
             !MerkleProof.verify(merkleProof, sessionKeyStorage.merkleRoot, leaf)
         ) {
-            revert("SKM: SessionNotApproved");
+            revert("SessionNotApproved");
         }
     }
 
