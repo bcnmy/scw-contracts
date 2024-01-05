3041d8f8406a45009f460d4d625fa969f57301d3
Ankur Dubey <iam@ankurdubey.com>
2024-01-05 11:20:46 +0530

    feat: Solidity ^0.8.23 for SessionKeyManagerHybrid

--- contracts/smart-account/modules/SessionKeyManagers/SessionKeyManagerHybrid.sol
+++ contracts/smart-account/modules/SessionKeyManagers/SessionKeyManagerHybrid.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.23;
 
 /* solhint-disable function-max-lines*/
 /* solhint-disable ordering*/
--- contracts/smart-account/test/mocks/MockEthSender.sol
+++ contracts/smart-account/test/mocks/MockEthSender.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.23;
 
 import {ISmartAccount} from "../../interfaces/ISmartAccount.sol";
 
