38721b6db4f0b215318aa312d75ab36208075196
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-01-24 13:29:08 +0400

    update functions

--- contracts/smart-account/test/mocks/BlastToken1.sol
+++ contracts/smart-account/test/mocks/BlastToken1.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../../interfaces/IBlast.sol";
 
 contract BlastToken1 is ERC20 {
-
     address public constant BLAST = 0x4300000000000000000000000000000000000002;
 
     constructor() ERC20("TST1", "MockToken") {
@@ -18,25 +17,23 @@ contract BlastToken1 is ERC20 {
     }
 
     function claimYield(address recipient, uint256 amount) external {
-	  //This function is public meaning anyone can claim the yield
-		IBlast(BLAST).claimYield(address(0), recipient, amount);
+        //This function is public meaning anyone can claim the yield
+        IBlast(BLAST).claimYield(address(this), recipient, amount);
     }
 
-	function claimAllYield(address recipient) external {
-	  //This function is public meaning anyone can claim the yield
-		IBlast(BLAST).claimAllYield(address(0), recipient);
+    function claimAllYield(address recipient) external {
+        //This function is public meaning anyone can claim the yield
+        IBlast(BLAST).claimAllYield(address(this), recipient);
     }
 
     function claimAllGas(address recipient) external {
-	    // This function is public meaning anyone can claim the gas
-		IBlast(BLAST).claimAllGas(address(0), recipient);
+        // This function is public meaning anyone can claim the gas
+        IBlast(BLAST).claimAllGas(address(this), recipient);
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 6;
     }
 
-    receive() external payable {
-    }
-
+    receive() external payable {}
 }
